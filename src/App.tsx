05e8b8d25e98a454ad3c351aa327e84a74558e31
Cristian Fleita <cristianagustinfleita@gmail.com>
2025-02-16 01:31:16 -0300

    chore: typescript and fonts

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import "./App.scss";
+import Nav from "./Components/Nav";
+import Div1 from "./Components/Div1";
+import Div2 from "./Components/Div2";
+import Div3 from "./Components/Div3";
+import Div4 from "./Components/Div4";
+import Div5 from "./Components/Div5";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Div1 />
+      <Div2 />
+      <Div3 />
+      <Div4 />
+      <Div5 />
+    </>
+  );
+}
+
+export default App;
