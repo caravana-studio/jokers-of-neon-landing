d1025676451d5404888cbd3864e5371c0377c1cf
Cristian Fleita <cristianagustinfleita@gmail.com>
2025-02-19 01:50:02 -0300

    chore: wip box section

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Div4 from "./Components/Div4";
 import Div5 from "./Components/Div5";
 import { HomeSection } from "./Components/HomeSection";
 import { CardsSection } from "./Components/CardsSection";
+import { BoxSection } from "./Components/BoxSection";
+import Div2 from "./Components/Div2";
 
 function App() {
   const theme = extendTheme(customTheme);
@@ -14,10 +16,8 @@ function App() {
     <>
       <ChakraBaseProvider theme={theme}>
         <HomeSection />
-        {/* <Nav />
-        <Div1 /> */}
         <CardsSection />
-        {/* <Div2 />  */}
+        <BoxSection />
         <Div3 />
         <Div4 />
         {/* <Div5 /> */}
