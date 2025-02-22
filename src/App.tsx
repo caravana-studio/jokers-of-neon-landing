42fba8987499afc81b3f39e91d987a0ec7260215
Cristian Fleita <cristianagustinfleita@gmail.com>
2025-02-22 01:19:22 -0300

    chore: footer desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import "./App.scss";
 import { ChakraBaseProvider, extendTheme } from "@chakra-ui/react";
 import customTheme from "./theme/theme";
-import Div4 from "./Components/Div4";
-import Div5 from "./Components/Div5";
 import { HomeSection } from "./Components/HomeSection";
 import { CardsSection } from "./Components/CardsSection";
 import { BoxSection } from "./Components/BoxSection";
 import { DemoSection } from "./Components/DemoSection";
+import { FooterSection } from "./Components/FooterSection";
 
 function App() {
   const theme = extendTheme(customTheme);
@@ -18,8 +17,7 @@ function App() {
         <CardsSection />
         <BoxSection />
         <DemoSection />
-        {/* <Div4 /> */}
-        {/* <Div5 /> */}
+        <FooterSection />
       </ChakraBaseProvider>
     </>
   );
