6e79182ac7fba60f3c44dab15f46192ed55ff008
Cristian Fleita <cristianagustinfleita@gmail.com>
2025-02-28 12:37:00 -0300

    chore: add title on the swipe slide

--- src/Components/BoxSection.tsx
+++ src/Components/BoxSection.tsx
@@ -46,9 +46,8 @@ export const BoxSection = () => {
     <Flex
       flexDirection={"column"}
       justifyContent={"center"}
-      minHeight={isMobile ? "80%" : "unset"}
-      height={isMobile ? "unset" : "80%"}
       pt={"5%"}
+      pb={isMobile ? "5%" : 0}
     >
       <Flex
         flexDirection={"column"}
@@ -274,45 +273,39 @@ export const BoxSection = () => {
                   },
                 }}
               >
-                DESCRIPTION:
+                CARD TYPE:
               </Text>
               <Text color={neonGreen} fontSize={{ base: "md", sm: "xl" }}>
-                More chances of getting a special card.
+                SPECIAL
               </Text>
             </Box>
 
-            {details && (
-              <Box mb={4}>
-                <Text
-                  color="white"
-                  fontSize={{ base: "md", sm: "lg" }}
-                  mb={2}
-                  sx={{
-                    position: "relative",
-                    _before: {
-                      content: '""',
-                      position: "absolute",
-                      bottom: 0,
-                      width: "95%",
-                      height: "2px",
-                      backgroundColor: "white",
-                      boxShadow:
-                        "0px 0px 12px rgba(255, 255, 255, 0.8), 0px 6px 20px rgba(255, 255, 255, 0.5)",
-                    },
-                  }}
-                >
-                  DETAILS:
-                </Text>
-                <Text color={neonGreen} fontSize={{ base: "md", sm: "xl" }}>
-                  {details?.split("\n").map((line, index) => (
-                    <span key={index}>
-                      {line}
-                      <br />
-                    </span>
-                  ))}
-                </Text>
-              </Box>
-            )}
+            <Box mb={4}>
+              <Text
+                color="white"
+                fontSize={{ base: "md", sm: "lg" }}
+                mb={2}
+                sx={{
+                  position: "relative",
+                  _before: {
+                    content: '""',
+                    position: "absolute",
+                    bottom: 0,
+                    width: "95%",
+                    height: "2px",
+                    backgroundColor: "white",
+                    boxShadow:
+                      "0px 0px 12px rgba(255, 255, 255, 0.8), 0px 6px 20px rgba(255, 255, 255, 0.5)",
+                  },
+                }}
+              >
+                DESCRIPTION:
+              </Text>
+              <Text color={neonGreen} fontSize={{ base: "md", sm: "xl" }}>
+                Score double points and multi
+              </Text>
+            </Box>
+
             <Box flex={1} alignItems={"end"} display={"flex"} flexDir={"row"}>
               <Flex flexDirection={"column"} gap={5}>
                 <Flex gap={3}>
@@ -367,91 +360,7 @@ export const BoxSection = () => {
       overflow={"hidden"}
       bgColor={"black"}
     >
-      {/* Title */}
-      <Flex
-        position="relative"
-        pt={isMobile ? 3 : 100}
-        justify={"center"}
-        alignItems={"center"}
-        flexDir={"column"}
-      >
-        <Box
-          position="absolute"
-          top="32%"
-          right={isMobile ? "50%" : "30%"}
-          width={"100%"}
-        >
-          <motion.img
-            width={isMobile ? "6%" : "3%"}
-            src="/elements/star.png"
-            alt="Rotating Star"
-            style={{
-              position: "absolute",
-              bottom: "10%",
-              left: isMobile ? "70%" : "65%",
-            }}
-            initial={{ rotate: 0 }}
-            whileInView={{ rotate: [0, -15, 15, 0] }}
-            transition={{ duration: 4, repeat: Infinity, ease: "easeInOut" }}
-          />
-
-          <motion.img
-            width={isMobile ? "15%" : "12%"}
-            src="/elements/ring.png"
-            alt="Rotating Ring"
-            style={{ position: "absolute", left: isMobile ? "70%" : "64%" }}
-            initial={{ rotate: 0 }}
-            whileInView={{ rotate: [0, 10, -10, 0] }}
-            transition={{ duration: 4, repeat: Infinity, ease: "easeInOut" }}
-          />
-        </Box>
-
-        <Box
-          position="absolute"
-          top={isMobile ? "10%" : "40%"}
-          right={isMobile ? "5%" : "30%"}
-        >
-          <Image width={"50%"} src="/elements/sparkles-crop-1.png"></Image>
-        </Box>
-
-        <motion.div
-          whileInView={{ opacity: [0, 1] }}
-          transition={{ duration: 1.5 }}
-        >
-          <Text
-            fontSize={{ base: "m", md: "m", xl: "2rem", xxl: "3rem" }}
-            letterSpacing="widest"
-            color="white"
-            fontFamily="Orbitron"
-          >
-            STEP INTO
-          </Text>
-        </motion.div>
-
-        <motion.div
-          whileInView={{ opacity: [0, 1], y: [20, 0] }}
-          transition={{ duration: 1.5 }}
-        >
-          <Text
-            fontSize={{
-              base: "3xl",
-              md: "3.5rem",
-              xl: "4rem",
-              xxl: "7rem",
-            }}
-            fontWeight="bold"
-            textTransform="uppercase"
-            textShadow="0 0 15px rgba(255, 255, 255, 0.9)"
-            letterSpacing="widest"
-            fontFamily="Orbitron"
-          >
-            STRATEGY
-          </Text>
-        </motion.div>
-      </Flex>
-
       <Flex position={"relative"}>
-        {/* Box preview */}
         <Swiper
           modules={[Navigation, Pagination, Autoplay]}
           spaceBetween={30}
@@ -464,10 +373,111 @@ export const BoxSection = () => {
           autoplay={{ delay: 5000, disableOnInteraction: false }}
           loop={true}
         >
-          <SwiperSlide>{specialBox}</SwiperSlide>
+          <SwiperSlide>
+            {/* Title */}
+            <Flex
+              position="relative"
+              pt={isMobile ? 3 : 100}
+              justify={"center"}
+              alignItems={"center"}
+              flexDir={"column"}
+            >
+              <Box
+                position="absolute"
+                top="32%"
+                right={isMobile ? "50%" : "30%"}
+                width={"100%"}
+              >
+                <motion.img
+                  width={isMobile ? "6%" : "3%"}
+                  src="/elements/star.png"
+                  alt="Rotating Star"
+                  style={{
+                    position: "absolute",
+                    bottom: "10%",
+                    left: isMobile ? "70%" : "65%",
+                  }}
+                  initial={{ rotate: 0 }}
+                  whileInView={{ rotate: [0, -15, 15, 0] }}
+                  transition={{
+                    duration: 4,
+                    repeat: Infinity,
+                    ease: "easeInOut",
+                  }}
+                />
+
+                <motion.img
+                  width={isMobile ? "15%" : "12%"}
+                  src="/elements/ring.png"
+                  alt="Rotating Ring"
+                  style={{
+                    position: "absolute",
+                    left: isMobile ? "70%" : "64%",
+                  }}
+                  initial={{ rotate: 0 }}
+                  whileInView={{ rotate: [0, 10, -10, 0] }}
+                  transition={{
+                    duration: 4,
+                    repeat: Infinity,
+                    ease: "easeInOut",
+                  }}
+                />
+              </Box>
+
+              <Box
+                position="absolute"
+                top={isMobile ? "10%" : "40%"}
+                right={isMobile ? "5%" : "30%"}
+              >
+                <Image
+                  width={"50%"}
+                  src="/elements/sparkles-crop-1.png"
+                ></Image>
+              </Box>
+
+              <motion.div
+                whileInView={{ opacity: [0, 1] }}
+                transition={{ duration: 1.5 }}
+              >
+                <Text
+                  fontSize={{ base: "m", md: "m", xl: "2rem", xxl: "3rem" }}
+                  letterSpacing="widest"
+                  color="white"
+                  fontFamily="Orbitron"
+                >
+                  STEP INTO
+                </Text>
+              </motion.div>
+
+              <motion.div
+                whileInView={{ opacity: [0, 1], y: [20, 0] }}
+                transition={{ duration: 1.5 }}
+              >
+                <Text
+                  fontSize={{
+                    base: "3xl",
+                    md: "3.5rem",
+                    xl: "4rem",
+                    xxl: "7rem",
+                  }}
+                  fontWeight="bold"
+                  textTransform="uppercase"
+                  textShadow="0 0 15px rgba(255, 255, 255, 0.9)"
+                  letterSpacing="widest"
+                  fontFamily="Orbitron"
+                >
+                  STRATEGY
+                </Text>
+              </motion.div>
+            </Flex>
+
+            {/* Box preview */}
+            {specialBox}
+          </SwiperSlide>
           <SwiperSlide>{jokerCard}</SwiperSlide>
         </Swiper>
 
+        {/* Swiper navigation arrows */}
         <Box
           className="swiper-button-prev"
           position="absolute"
@@ -489,9 +499,6 @@ export const BoxSection = () => {
           <ChevronRightIcon boxSize={10} color="white" />
         </Box>
       </Flex>
-
-      {/* icons */}
-      <Flex></Flex>
     </Box>
   );
 };
