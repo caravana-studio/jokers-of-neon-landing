aa2d586da162d7cd5fb270f14c75386399a05686
Cristian Fleita <cristianagustinfleita@gmail.com>
2025-02-28 16:37:56 -0300

    chore: wip adjust box

--- src/Components/BoxSection.tsx
+++ src/Components/BoxSection.tsx
@@ -11,7 +11,7 @@ import "swiper/css/pagination";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
 
-const SIZE_MULTIPLIER = isMobile ? 0.5 : 2;
+const SIZE_MULTIPLIER = 2;
 const { white, neonGreen } = theme.colors;
 
 const animationsData = {
@@ -51,14 +51,15 @@ export const BoxSection = () => {
     >
       <Flex
         flexDirection={"column"}
-        justifyContent={"center"}
-        width={{ base: "85%", sm: "60%" }}
+        alignItems={"flex-end"}
+        width={{ base: "85%", sm: "50%" }}
         margin={"0 auto"}
         bg="rgba(0, 0, 0, 0.6)"
         borderRadius="25px"
         p={6}
         boxShadow={`0px 0px 10px 1px ${white}`}
         zIndex={1}
+        height={isMobile ? "80vh" : "100%"}
       >
         <Flex
           flexDirection={{ base: "column", sm: "row" }}
@@ -66,68 +67,52 @@ export const BoxSection = () => {
           gap={4}
           flex="1"
           height="100%"
+          width="100%"
+          justify={"flex-end"}
+          position={"relative"}
         >
           <Flex
+            position={isMobile ? "unset" : "absolute"}
+            top={isMobile ? "5%" : 0}
+            left={isMobile ? "0" : "-15%"}
             w={{ base: "100%", sm: "35%" }}
-            h={`${CARD_HEIGHT * SIZE_MULTIPLIER + 30}px`}
+            h={{
+              base: `${CARD_HEIGHT * 2 + 30}px`,
+              xl: `${CARD_HEIGHT * 3 + 30}px`,
+            }}
             justifyContent="center"
             flexDir="column"
           >
             {spineAnim}
           </Flex>
+          <Flex width={isMobile ? "100%" : "75%"}>
+            <Flex
+              flexDirection={"column"}
+              ml={{ base: "15px", sm: "30px" }}
+              flex="1"
+              height="100%"
+              justifyContent={"space-between"}
+            >
+              <Flex justifyContent="space-between" alignItems="center">
+                <Heading
+                  fontSize={{ base: "sm", sm: "md", xl: "2.5rem", xxl: "4rem" }}
+                  variant="italic"
+                >
+                  SPECIAL PACKS
+                </Heading>
+                {!isMobile && (
+                  <Image
+                    src={`/logos/jn-logo.png`}
+                    alt={"JN logo"}
+                    width="120px"
+                  />
+                )}
+              </Flex>
 
-          <Flex
-            flexDirection={"column"}
-            width="100%"
-            ml={{ base: "15px", sm: "30px" }}
-            flex="1"
-            height="100%"
-            justifyContent={"space-between"}
-          >
-            <Flex justifyContent="space-between" alignItems="center">
-              <Heading size={{ base: "sm", sm: "l" }} variant="italic">
-                SPECIAL PACKS
-              </Heading>
-              {!isMobile && (
-                <Image
-                  src={`/logos/jn-logo.png`}
-                  alt={"JN logo"}
-                  width="120px"
-                />
-              )}
-            </Flex>
-
-            <Box mb={4}>
-              <Text
-                color="white"
-                fontSize={{ base: "md", sm: "lg" }}
-                mb={2}
-                sx={{
-                  position: "relative",
-                  _before: {
-                    content: '""',
-                    position: "absolute",
-                    bottom: 0,
-                    width: "95%",
-                    height: "2px",
-                    backgroundColor: "white",
-                    boxShadow:
-                      "0px 0px 12px rgba(255, 255, 255, 0.8), 0px 6px 20px rgba(255, 255, 255, 0.5)",
-                  },
-                }}
-              >
-                DESCRIPTION:
-              </Text>
-              <Text color={neonGreen} fontSize={{ base: "md", sm: "xl" }}>
-                More chances of getting a special card.
-              </Text>
-            </Box>
-
-            {details && (
               <Box mb={4}>
                 <Text
                   color="white"
-                  fontSize={{ base: "md", sm: "lg" }}
+                  fontSize={{ base: "md", sm: "lg", xl: "2rem", xxl: "3rem" }}
                   mb={2}
                   sx={{
                     position: "relative",
@@ -143,44 +128,93 @@ export const BoxSection = () => {
                     },
                   }}
                 >
-                  DETAILS:
+                  DESCRIPTION:
                 </Text>
-                <Text color={neonGreen} fontSize={{ base: "md", sm: "xl" }}>
-                  {details?.split("\n").map((line, index) => (
-                    <span key={index}>
-                      {line}
-                      <br />
-                    </span>
-                  ))}
+                <Text
+                  color={neonGreen}
+                  fontSize={{ base: "md", sm: "xl", xl: "1.5rem", xxl: "2rem" }}
+                >
+                  More chances of getting a special card.
                 </Text>
               </Box>
-            )}
-            <Box flex={1} alignItems={"end"} display={"flex"} flexDir={"row"}>
-              <Flex flexDirection={"column"} gap={5}>
-                <Flex gap={3}>
-                  <Flex gap={3}>
-                    <Heading
-                      fontSize={{ base: "sm", sm: "lg" }}
-                      variant="italic"
-                    >
-                      Price:
-                    </Heading>
-                    <Heading
-                      fontSize={{ base: "sm", sm: "lg" }}
-                      variant="italic"
-                      textDecoration={"none"}
-                    >
-                      750
-                      <span
-                        style={{ fontFamily: "Orbitron", marginLeft: "3px" }}
-                      >
-                        ¢
+
+              {details && (
+                <Box mb={4}>
+                  <Text
+                    color="white"
+                    fontSize={{ base: "md", sm: "lg", xl: "2rem", xxl: "3rem" }}
+                    mb={2}
+                    sx={{
+                      position: "relative",
+                      _before: {
+                        content: '""',
+                        position: "absolute",
+                        bottom: 0,
+                        width: "95%",
+                        height: "2px",
+                        backgroundColor: "white",
+                        boxShadow:
+                          "0px 0px 12px rgba(255, 255, 255, 0.8), 0px 6px 20px rgba(255, 255, 255, 0.5)",
+                      },
+                    }}
+                  >
+                    DETAILS:
+                  </Text>
+                  <Text
+                    color={neonGreen}
+                    fontSize={{
+                      base: "md",
+                      sm: "xl",
+                      xl: "1.5rem",
+                      xxl: "2rem",
+                    }}
+                  >
+                    {details?.split("\n").map((line, index) => (
+                      <span key={index}>
+                        {line}
+                        <br />
                       </span>
-                    </Heading>
+                    ))}
+                  </Text>
+                </Box>
+              )}
+              <Box flex={1} alignItems={"end"} display={"flex"} flexDir={"row"}>
+                <Flex flexDirection={"column"} gap={5}>
+                  <Flex gap={3}>
+                    <Flex gap={3}>
+                      <Heading
+                        fontSize={{
+                          base: "sm",
+                          sm: "lg",
+                          xl: "1.5rem",
+                          xxl: "2rem",
+                        }}
+                        variant="italic"
+                      >
+                        Price:
+                      </Heading>
+                      <Heading
+                        fontSize={{
+                          base: "sm",
+                          sm: "lg",
+                          xl: "1.5rem",
+                          xxl: "2rem",
+                        }}
+                        variant="italic"
+                        textDecoration={"none"}
+                      >
+                        750
+                        <span
+                          style={{ fontFamily: "Orbitron", marginLeft: "3px" }}
+                        >
+                          ¢
+                        </span>
+                      </Heading>
+                    </Flex>
                   </Flex>
                 </Flex>
-              </Flex>
-            </Box>
+              </Box>
+            </Flex>
           </Flex>
         </Flex>
       </Flex>
@@ -370,7 +404,7 @@ export const BoxSection = () => {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
           }}
-          autoplay={{ delay: 5000, disableOnInteraction: false }}
+          // autoplay={{ delay: 5000, disableOnInteraction: false }}
           loop={true}
         >
           <SwiperSlide>
