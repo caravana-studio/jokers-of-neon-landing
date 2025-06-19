6b9761ad60034bb821a0057899aee909ba21e34c
nicon44 <nicolas@dulio.com.ar>
2025-06-19 18:53:17 +0200

    replace gg for alpha

--- src/Components/FullScreenAlpha.tsx
+++ src/Components/FullScreenAlpha.tsx
@@ -58,7 +58,7 @@ export const FullScreenAlpha = ({ onClose }: FullScreenAlphaProps) => {
           </Button>
         </Link>
         <Link
-          href="https://gg.jokersofneon.com/"
+          href="https://alpha.jokersofneon.com/"
           target="_blank"
           textDecoration="none !important"
         >
